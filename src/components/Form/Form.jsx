7895b549b921960fec54d603838b3be2cd561763
Xeveryeo <143580217+Xeveryeo@users.noreply.github.com>
2024-06-23 13:07:18 +0800

    Create Form.jsx

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,67 @@
+import styles from './form.module.css';
+import { Component } from 'react';
+
+class Form extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+  handleChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { name } = this.state;
+    const toLowerName = name.toLowerCase();
+    const doubleName = this.props.doubleContact(toLowerName);
+    doubleName
+      ? alert(`${name} is already in contacts`)
+      : this.props.onSubmit(this.state);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    const { name, number } = this.state;
+    return (
+      <form className={styles.form} onSubmit={this.handleSubmit}>
+        <label className={styles.label}>
+          <span className={styles.span}>Name</span>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={this.handleChange}
+          />
+        </label>
+        <label className={styles.label}>
+          <span className={styles.span}>Tel</span>
+          <input
+            type="tel"
+            name="number"
+            value={number}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            onChange={this.handleChange}
+          />
+        </label>
+        <button className={styles.btn} type="submit">
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Form;
